b96699fc35f2c908356d9332c9bec0c663c440a5
Teo_You <teoyou881@gmail.com>
2025-05-10 19:45:46 -0400

    first commit

---
+++ src/test/java/spring/data_jpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
